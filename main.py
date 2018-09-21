6cf1e87674282e3dbe47a9e137510603ef65b6b9
Boris Dimitrov <bdimitrov@chanzuckerberg.com>
2018-09-20 18:52:14 -0700

    reweight abundance by organism (#11)
    also fix exception handler and bump to v4

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ from genome import Genome
 
 # Increment this as often as you like;  especially if a code change will result
 # in different content for the same output filename.
-LOGICAL_VERSION = "3"
+LOGICAL_VERSION = "4"
 
 
-def augmented_read_header(line, r, line_number):
+def augment_read_header(line, r, line_number):
     assert line.endswith(r), f"fastq produced by ISS have read id's ending with _1\\n or _2\\n"
     assert len(r) == 3
     iss_read_id = line[:-3]
@@ -45,21 +45,26 @@ def annotate_reads_work(input_f, output_f, r):
             # The FASTQ format specifies that each read consists of 4 lines,
             # the first of which begins with @ followed by read ID.
             assert line[0] == "@", f"fastq format requires every 4th line to start with @"
-            output_f.write(augmented_read_header(line, r, line_number).encode('utf-8'))
+            augmented_read_header = augment_read_header(line, r, line_number)
+            output_f.write(augmented_read_header.encode('utf-8'))
             for i in range(4):
                 line = input_f.readline()
                 line_number += 1
                 if i < 3:
                     output_f.write(line.encode('utf-8'))
-    except:
-        print(f"Error parsing line {line_number} in {input_fastq}.")
+    except Exception as e:
+        e.line_number = line_number
         raise
 
 
 def annotate_reads(input_fastq, output_fastq, r):
     with smart_open(input_fastq, "r") as input_f:
         with smart_open(output_fastq, "w") as output_f:
-            annotate_reads_work(input_f, output_f, r)
+            try:
+                annotate_reads_work(input_f, output_f, r)
+            except Exception as e:
+                print(f"Error parsing line {e.line_number} in {input_fastq}.")
+                raise
 
 
 def main():
@@ -67,18 +72,25 @@ def main():
     num_cpus = cpu_count()
     num_reads = 100 * 1000
     model = MODELS[0]
-    abundance = ABUNDANCES[0]
     genome_fastas = " ".join(g.filename for g in TOP_6_ID_GENOMES)
     Genome.ensure_all_present()
     num_organisms = len(TOP_6_ID_GENOMES)
     num_accessions = sum(len(g.versioned_accession_ids) for g in TOP_6_ID_GENOMES)
-    output_prefix = f"norg_{num_organisms}__nacc_{num_accessions}__{abundance}_weight_per_accession__{model}_reads__v{LOGICAL_VERSION}_"
+    output_prefix = f"norg_{num_organisms}__nacc_{num_accessions}__uniform_weight_per_organism__{model}_reads__v{LOGICAL_VERSION}_"
     pid = os.getpid()
     tmp_prefix = f"tmp_{pid}"
     tmp_files = [f"{tmp_prefix}_{r}.fastq" for r in ["R1", "R2"]]
     output_files = [f"{output_prefix}_{r}.fastq.gz" for r in ["R1", "R2"]]
-    for f in tmp_files + output_files:
+    abundance_file = f"{tmp_prefix}_abundance.txt"
+    for f in tmp_files + output_files + [abundance_file]:
         remove_safely(f)
+    with open(abundance_file, "w") as af:
+        sum_of_weights = 0
+        for vaccid, genome in Genome.by_accid.items():
+            vaccid_weight = 1.0 / (len(genome.versioned_accession_ids) * num_organisms)
+            sum_of_weights += vaccid_weight
+            af.write(f"{vaccid} {vaccid_weight}\n")
+    assert -0.0005 < sum_of_weights - 1.0 < 0.0005, f"{sum_of_weights} != 1.0"
     # TODO:  Currently each chromosome is treated as a separate organism
     # for relative abundance purposes.  Thus, organisms with greater number
     # of chromosomes will have a lot of extra weight in the mix.
@@ -86,11 +98,12 @@ def main():
     remove_safely("top_6_pathogens.fasta")
     command = f"cat {genome_fastas} > top_6_pathogens.fasta"
     check_call(command)
-    command = f"iss generate --n_reads {num_reads} --genomes top_6_pathogens.fasta --model {model} --abundance {abundance} --gc_bias --output {tmp_prefix} --cpus {num_cpus}"
+    command = f"iss generate --n_reads {num_reads} --genomes top_6_pathogens.fasta --model {model} --abundance_file {abundance_file} --gc_bias --output {tmp_prefix} --cpus {num_cpus}"
     check_call(command)
     for tmp_fastq, output_fastq, r in zip(tmp_files, output_files, ["_1\n", "_2\n"]):
         annotate_reads(tmp_fastq, output_fastq, r)
         remove_safely(tmp_fastq)
+    remove_safely(abundance_file)
 
 
 if __name__ == "__main__":
